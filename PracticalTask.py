0f38b04fd443ce8aa641802e424538a6ff29cb8e
chuva40k <pechalka345@gmail.com>
2026-02-03 13:31:18 +0200

    adfda

---
+++ PracticalTask.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("Проект Python")
+root.geometry("400x300")
+def button1_click():
+    root.geometry("800x400")
+    root.configure(bg="green")
+    btn1.config(width=30, bg="blue", fg="white", state="disabled")
+    current_width = btn2.cget("width")
+    btn2.config(width=current_width + 5)
+
+
+
+def button2_click():
+
+    label.config(
+        bg="red",
+        width=40,
+        fg="yellow",
+        text="Ми вивчаємо мову програмування Python!"
+    )
+
+
+
+btn1 = tk.Button(
+    root,
+    text="Вікно проекту",
+    bg="grey",
+    fg="yellow",
+    width=20,
+    height=2,
+    command=button1_click
+)
+btn1.pack(pady=10)
+
+
+btn2 = tk.Button(
+    root,
+    text="Напис",
+    bg="blue",
+    fg="white",
+    width=10,
+    height=3,
+    command=button2_click  # прив'язка обробника події
+)
+btn2.pack(pady=10)
+
+# 4. Створення напису
+# Замість "10-А клас" ви можете вписати свою назву класу
+label = tk.Label(
+    root,
+    text="11-А1 клас",
+    fg="blue",
+    font=("Arial", 14)
+)
+label.pack(pady=10)
+
+root.mainloop()
