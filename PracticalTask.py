0eaad4a4c32e5b31e7db803122451728a4c3b181
chuva40k <pechalka345@gmail.com>
2026-02-17 12:51:00 +0200

    fafa

--- PracticalTask.py
+++ PracticalTask.py
@@ -1,56 +1,26 @@
-import tkinter as tk
-root = tk.Tk()
-root.title("Проект Python")
-root.geometry("400x300")
-def button1_click():
-    root.geometry("800x400")
-    root.configure(bg="green")
-    btn1.config(width=30, bg="blue", fg="white", state="disabled")
-    current_width = btn2.cget("width")
-    btn2.config(width=current_width + 5)
-
-
-
-def button2_click():
-
-    label.config(
-        bg="red",
-        width=40,
-        fg="yellow",
-        text="Ми вивчаємо мову програмування Python!"
-    )
-
-
-
-btn1 = tk.Button(
-    root,
-    text="Вікно проекту",
-    bg="grey",
-    fg="yellow",
-    width=20,
-    height=2,
-    command=button1_click
-)
-btn1.pack(pady=10)
-
-
-btn2 = tk.Button(
-    root,
-    text="Напис",
-    bg="blue",
-    fg="white",
-    width=10,
-    height=3,
-    command=button2_click
-)
-btn2.pack(pady=10)
-
-label = tk.Label(
-    root,
-    text="11-А1 клас",
-    fg="blue",
-    font=("Arial", 14)
-)
-label.pack(pady=10)
-
-root.mainloop()
+#ПОВНІСТЮ ГОТОВА ЄБОТНЯ
+from tkinter import *
+tk = Tk()
+tk.geometry('400x300')
+tk.title("Проект")
+def button1_clicked():
+    tk.geometry('800x400')
+    tk['bg'] = 'green'
+    button1['width'] = 30
+    button1['bg'] = 'blue'
+    button1['fg'] = 'white'
+    button1['state'] = 'disabled'
+    current_width = int(button2['width'])
+    button2['width'] = current_width + 5
+def button2_clicked():
+    label['bg'] = 'red'
+    label['width'] = 40
+    label['fg'] = 'yellow'
+    label['text'] = "Ми вивчаємо мову програмування Python!"
+button1 = Button(tk, text="Вікно проекту", width=20, height=2, bg="grey", fg="yellow", command=button1_clicked)
+button1.place(x=50, y=50)
+button2 = Button(tk, text="Напис", width=10, height=3, bg="blue", fg="white", command=button2_clicked)
+button2.place(x=250, y=50)
+label = Label(tk, text="11-А2 клас", fg="blue", font=("Arial", 14))
+label.place(x=50, y=150)
+tk.mainloop()
--- script.py
+++ script.py
@@ -1,33 +1,64 @@
 from tkinter import *
+
 tk = Tk()
-tk.geometry("600x500")
-tk.title("Пес патрон")
+tk.geometry("600x600")
+tk.title("Вітальна листівка")
+
 def btn1_click():
-    index = listbox.curselection()[0]
-    greeting = listbox.get(index)
-    lbl_result.config(text=greeting + ", " + ent.get() + "!")
-def btn2_click():
-    print("Прощавай, ", ent.get(), "!", sep="")
+    global img
+    if var.get() == 1:
+        img = PhotoImage(file='flowers.png')
+    if var.get() == 2:
+        img = PhotoImage(file='cake.png')
+    if var.get() == 3:
+        img = PhotoImage(file='landscape.png')
+
+    select = list(lbox.curselection())
+    lbl2.config(text=ent.get() + ", " + lbox.get(select[0]))
+
+    s = canvas.create_image(25, 180, anchor=NW, image=img)
+
 lbl1 = Label(text="Ваше ім'я:")
-lbl1.place(x=75, y=25, width=100, height=30)
+lbl1.place(x=50, y=50)
+
 ent = Entry(bd=1)
-ent.place(x=225, y=25, width=100, height=30)
-btn1 = Button(text="Привітання", command=btn1_click)
-btn1.place(x=75, y=75, width=100, height=50)
-btn2 = Button(text="Прощавання", command=btn2_click)
-btn2.place(x=225, y=75, width=100, height=50)
-lbl_result = Label(text="")
-lbl_result.place(x=150, y=150)
-lbl_list = Label(text="Оберіть привітання:")
-lbl_list.place(x=350, y=25)
-listbox = Listbox()
-listbox.insert(1, "З 8 березня")
-listbox.insert(2, "З Новим роком")
-listbox.insert(3, "З Днем народження")
-listbox.insert(4, "Привіт")
-listbox.place(x=350, y=50, width=150, height=80)
-img = PhotoImage(file="1.png")
-lbl_img = Label(image=img)
-lbl_img.image = img
-lbl_img.place(x=75, y=200)
+ent.place(x=120, y=50, width=120)
+
+btn1 = Button(text="Привітання", command=btn1_click, bg="lightgrey")
+btn1.place(x=50, y=100, width=100, height=40)
+
+btn2 = Button(text="Вихід", command=tk.destroy, bg="lightgrey")
+btn2.place(x=180, y=100, width=100, height=40)
+
+lbl2 = Label(text="Тут буде привітання", font=("Arial", 12, "bold"))
+lbl2.place(x=50, y=160)
+
+lbl_list = Label(text="Вітання")
+lbl_list.place(x=350, y=30)
+
+lbox = Listbox(height=4)
+lbox.insert(1, "З Днем народження!")
+lbox.insert(2, "З Новим роком!")
+lbox.insert(3, "З 8 березня!")
+lbox.select_set(0)
+lbox.place(x=350, y=50, width=150)
+
+lbl_radio = Label(text="Зображення на листівку")
+lbl_radio.place(x=350, y=150)
+
+var = IntVar()
+var.set(1)
+
+r1 = Radiobutton(text="Квіти", variable=var, value=1)
+r1.place(x=350, y=180)
+
+r2 = Radiobutton(text="Торт", variable=var, value=2)
+r2.place(x=350, y=210)
+
+r3 = Radiobutton(text="Пейзаж", variable=var, value=3)
+r3.place(x=350, y=240)
+
+canvas = Canvas(tk, width=400, height=400, bg="white")
+canvas.place(x=50, y=200)
+
 tk.mainloop()
